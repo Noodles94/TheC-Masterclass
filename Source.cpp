a2e406dbbd7cd8c6e6c1134b4e423b7d48a35863
leo carlen <leoj_c@hotmail.com>
2022-08-11 17:54:05 +0200

    new stuff

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,41 @@
-#include <string>
 #include <iostream>
+#include <string>
+
+
+template<std::integral T>
+T add(T a, T b) {
+	return a + b;
+}
 template <typename T>
-concept convertible_to_std_string = requires (T a) {
-	std::to_string(a);
+requires std::integral<T> 
+T add4(T a, T b) {
+	return a + b;
+}
+
+auto add2(std::integral auto a, std::integral auto b) {
+	return a + b;
+}
+
+template<typename T>
+T add3(T a, T b) requires std::is_integral_v<T> {
+	return a + b;
+}
+
+template <typename T>
+concept Multipliable = requires(T a, T b) {
+	a* b;
 };
 
 template <typename T>
-requires convertible_to_std_string<T>
-std::string concatenate(T first_value, T second_value) {
-	return std::to_string(first_value) + std::to_string(second_value);
+T multiply(T a, T b) requires Multipliable<T>{
+	return a * b;
 }
-
-
-
-
 int main() {
-	double a{ 22.22 };
-	double b{ 33.33 };
-	auto result = concatenate(22, 33);
-	std::cout << result << std::endl;
-	std::cout << concatenate(a, b);
+
+	int a{ 2 }, b{ 3 };
+	std::string string_a{ "Hello" };
+	//std::cout << add(a, b);
+	//std::cout << add(string_a, string_a);
+	std::cout << add2('a', 2);
 	return 0;
 }
---
+++ assignment_11_put_em_strings_together.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+template <typename T>
+concept convertible_to_std_string = requires (T a) {
+	std::to_string(a);
+};
+
+template <typename T>
+requires convertible_to_std_string<T>
+std::string concatenate(T first_value, T second_value) {
+	return std::to_string(first_value) + std::to_string(second_value);
+}
+
+
+
+
+int put_em_stdstrings_tog() {
+	double a{ 22.22 };
+	double b{ 33.33 };
+	auto result = concatenate(22, 33);
+	std::cout << result << std::endl;
+	std::cout << concatenate(a, b);
+	return 0;
+}
